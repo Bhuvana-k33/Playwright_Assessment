5e7cff70cc631152d5dc9eda668628e89feafdfe
Bhuvana-k33 <k.bhuvaneshwari@unthinkable.co>
2026-01-11 17:10:09 +0530

    Assignment_2

--- test_fileupload.py
+++ test_fileupload.py
@@ -8,4 +8,9 @@ def test_fileupload(page: Page):
     page.goto("https://the-internet.herokuapp.com/upload")
     page.get_by_role("heading", name="File Uploader").click()
     page.set_input_files("input[type='file']",r"C:\Users\Daffolap-1080\PycharmProjects\PythonProject\File_upload.txt")
+    page.click("#file-submit")
+
+    file_name=page.locator("#uploaded-files").text_content()
+    assert "File_upload.txt" in file_name
+    print("File name matched")
     time.sleep(5)
